c7e1ce2bd59b13de2d27a48ad39aeab8987a0475
root <fabius8@163.com>
2020-10-13 10:17:56 +0800

    huobi binance ok

--- contract/management/commands/getinfo.py
+++ contract/management/commands/getinfo.py
@@ -3,6 +3,7 @@ from contract.models import Tradepair
 import ccxt
 import json
 import time
+import requests
 
 config = json.load(open('config_funding.json'))
 binance_future = ccxt.binance(config["binance_future"])
@@ -14,6 +15,9 @@ okex_future = ccxt.okex(config["okex_future"])
 okex_spot = ccxt.okex(config["okex_spot"])
 okex_future.load_markets()
 okex_spot.load_markets()
+
+huobi_spot = ccxt.huobipro()
+huobi_spot.load_markets()
 
 swapusd = []
 swapusdt = []
@@ -30,9 +34,10 @@ swap = swapusd + swapusdt
 class Command(BaseCommand):
     def handle(self, *args, **options):
         while True:
+            update_huobi_info()
             update_okex_info()
             update_binance_info()
-            time.sleep(60)
+            time.sleep(5)
 
 
 def get_okex_spread_close(symbol, future, spot):
@@ -55,7 +60,7 @@ def get_okex_spread_close(symbol, future, spot):
     return (((bid0_A - ask0_B)/ask0_B*100), ((bid0_B - ask0_A)/ask0_A*100), bid0_A, bid0_B)
 
 
-def get_spread_close(symbol, future, spot):
+def get_binance_spread_close(symbol, future, spot):
     try:
         order_book_A = future.fetch_order_book(symbol)
         bid0_A = order_book_A['bids'][0][0]
@@ -77,10 +82,8 @@ def update_okex_info():
             spread = get_okex_spread_close(i, okex_future, okex_spot)
             result = Tradepair.objects.filter(symbol=i, exchange="okex")
             if result.exists():
-                print(i, "QuerySet has Data")
                 tradepair = Tradepair.objects.get(symbol=i, exchange="okex")
             else:
-                print(i, "QuerySet empty")
                 tradepair = Tradepair()
             tradepair.symbol = i
             tradepair.futureprice = spread[2]
@@ -91,7 +94,6 @@ def update_okex_info():
             tradepair.sellSpread = float('%.2f' % spread[0])
             tradepair.buySpread = float('%.2f' % spread[1])
             tradepair.save()
-            print(i)
 
     except Exception as err:
         print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), "update okex fail", err)
@@ -104,12 +106,10 @@ def update_binance_info():
         for i in fundingRate:
             result = Tradepair.objects.filter(symbol=i['symbol'], exchange="binance")
             if result.exists():
-                #print("QuerySet has Data")
                 tradepair = Tradepair.objects.get(symbol=i['symbol'], exchange="binance")
             else:
-                #print("QuerySet empty")
                 tradepair = Tradepair()
-            spread = get_spread_close(i['symbol'].replace('USDT', '/USDT'), binance_future, binance_spot)
+            spread = get_binance_spread_close(i['symbol'].replace('USDT', '/USDT'), binance_future, binance_spot)
             tradepair.symbol = i['symbol']
             tradepair.futureprice = spread[2]
             tradepair.spotprice = spread[3]
@@ -119,7 +119,52 @@ def update_binance_info():
             tradepair.sellSpread = float('%.2f' % spread[0])
             tradepair.buySpread = float('%.2f' % spread[1])
             tradepair.save()
+
     except Exception as err:
         print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), err)
         pass
+
+
+def get_huobi_spread_close(future_symbol, spot_symbol, spot):
+    try:
+        r = requests.get("https://api.hbdm.com/swap-ex/market/depth?contract_code=" + future_symbol + "&type=step0")
+        bid0_A = r.json()['tick']['bids'][0][0]
+        ask0_A = r.json()['tick']['asks'][0][0]
+        order_book_B = spot.fetch_order_book(spot_symbol)
+        bid0_B = order_book_B['bids'][0][0]
+        ask0_B = order_book_B['asks'][0][0]
+    except Exception as err:
+        print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), symbol, err)
+        return (0,0,0,0)
+        pass
+
+    return (((bid0_A - ask0_B)/ask0_B*100), ((bid0_B - ask0_A)/ask0_A*100), bid0_A, bid0_B)
+
+
+def update_huobi_info():
+    try:
+        r = requests.get("https://api.hbdm.com/swap-api/v1/swap_contract_info")
+        for i in r.json()['data']:
+            result = Tradepair.objects.filter(symbol=i['contract_code'], exchange="huobi")
+            if result.exists():
+                tradepair = Tradepair.objects.get(symbol=i['contract_code'], exchange="huobi")
+            else:
+                tradepair = Tradepair()
+            j = requests.get("https://api.hbdm.com/swap-api/v1/swap_funding_rate?contract_code=" + i['contract_code'])
+            spread = get_huobi_spread_close(i['contract_code'], i['contract_code'].replace('-USD', '/USDT'), huobi_spot)
+            tradepair.symbol = i['contract_code']
+            tradepair.futureprice = spread[2]
+            tradepair.spotprice = spread[3]
+            tradepair.exchange = "huobi"
+            tradepair.fundRate = float("%.2f" % (float(j.json()['data']['estimated_rate']) * 100))
+            tradepair.LastRate = float("%.2f" % (float(j.json()['data']['funding_rate']) * 100))
+            tradepair.sellSpread = float('%.2f' % spread[0])
+            tradepair.buySpread = float('%.2f' % spread[1])
+            tradepair.save()
+            print(i['symbol'], "OK")
+
+    except Exception as err:
+        print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), err)
+        pass
+
         
--- contract/views.py
+++ contract/views.py
@@ -9,9 +9,12 @@ from .tables import PairTable
 def pair(request):
     table1 = PairTable(Tradepair.objects.filter(Q(exchange="binance")&(Q(fundRate__gt=0.01)|Q(fundRate__lte=-0.06)|Q(buySpread__gte=0.3)|Q(sellSpread__gte=0.3))), prefix="1-")
     table2 = PairTable(Tradepair.objects.filter(Q(exchange="okex")&(Q(fundRate__gte=0.1)|Q(fundRate__lte=-0.1)|Q(buySpread__gte=0.5)|Q(sellSpread__gte=0.5))), prefix="2-")
+    table3 = PairTable(Tradepair.objects.filter(Q(exchange="huobi")&(Q(fundRate__gte=0.1)|Q(fundRate__lte=-0.1)|Q(buySpread__gte=0.5)|Q(sellSpread__gte=0.5))), prefix="3-")
     RequestConfig(request, paginate={"per_page": 10}).configure(table1)
     RequestConfig(request, paginate={"per_page": 10}).configure(table2)
+    RequestConfig(request, paginate={"per_page": 10}).configure(table3)
     return render(request, 'contract/pair.html', {
         'table1': table1,
         'table2': table2,
+        'table3': table3,
     })
